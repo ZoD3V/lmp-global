b632646bc3fcd9250799b349fd1f2cb1a47d7483
ZoD3V <adzkiyadzikrullah@gmail.com>
2025-01-08 09:14:29 +0700

    feat: adjustment table admin

---
+++ app/Filament/Resources/PermissionResource/Pages/EditPermission.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\PermissionResource\Pages;
+
+use App\Filament\Resources\PermissionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPermission extends EditRecord
+{
+    protected static string $resource = PermissionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Permission updated.';
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,20 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
         ]);
+        $role = Role::create(['name' => 'Admin']);
+        $user->assignRole($role);
+
+        $user1 = User::factory()->create([
+            'name' => 'Maintence',
+            'email' => 'maintence@maintence.com',
+            'password' => Hash::make('maintence'),
+        ]);
+        $role = Role::create(['name' => 'Maintained']);
+        $user1->assignRole($role);
     }
 }
