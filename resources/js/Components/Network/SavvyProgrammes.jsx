80b75d2fbedeba89580e0f08604d78293634a165
ZoD3V <adzkiyadzikrullah@gmail.com>
2025-04-15 11:54:25 +0700

    fix: fixing import

--- resources/js/Components/Network/SavvyProgrammes.jsx
+++ resources/js/Components/Network/SavvyProgrammes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import kementrian from "../../../../public/images/network/kementrianRepublikIndonesia.png";
 
 const SavvyProgrammes = () => {
